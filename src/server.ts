ea2a2a71fd12cceec4db7a759003aaf6876e3215
wayneleon1 <rurangwaleon10@gmail.com>
2024-04-18 17:28:38 +0200

    config index file

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,34 @@
-console.log("Hello world");
+import express, { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import JobRouter from "./routes/job.route";
+
+export const prisma = new PrismaClient();
+
+const app = express();
+const port = 8080;
+
+async function main() {
+  app.use(express.json());
+
+  // Register API routes
+  app.use("/api/v1/job", JobRouter);
+
+  // Catch unregistered routes
+  app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({ error: `Route ${req.originalUrl} not found` });
+  });
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$connect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
